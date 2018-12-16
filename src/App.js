45c5b9e56966bd4c29ffad9bca358080364fc5fd
Jared Giangrasso <jared@Jareds-Mac-mini.fios-router.home>
2018-12-16 14:59:32 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+const PATH_BASE = "https://reststop.randomhouse.com/resources/works/?start=0&max=10&search=dog";
+// const PARAM_AUTHOR = "authors?firstName=John"; //temp test link
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isbnList: {
+        title: [],
+        isbn: [],
+      },
+    };
+  }
+
+  componentDidMount() {
+    axios(`${PATH_BASE}`)
+      .then(res => {
+        const newIsbnList = res.data.work.map(work => {
+          console.log(work);
+          var newIsbn = Array.from(work.titles.isbn);
+          return newIsbn.map(isbn => {
+            return {
+              title: work.titleweb,
+              isbn: isbn['$'],
+            };
+          })
+        });
+
+        // const flatIsbnList = newIsbnList.flat();
+        const newState = Object.assign(this.state, {
+          title: newIsbnList,
+        });
+
+        this.setState({ newState }, console.log(this.state));
+
+      })
+
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Covers isbnList={ this.state.isbnList } />
       </div>
     );
   }
 }
+
+const Covers = props =>
+<div>
+  <h1>{props[0]}</h1>
+</div>
+
+
 
 export default App;
